0249ffb1bcfabceb5b1400d5a817e129537bd1f9
gil <longlongroad@naver.com>
2023-01-18 23:24:25 +0900

    [add] redux 바닐라 스크립트로 데이터 스택 만들기(배열)

--- README.md
+++ README.md
@@ -90,3 +90,76 @@ const btnMinusHandler = () =>{
 add.addEventListener('click', btnPlusHandler);
 minus.addEventListener('click', btnMinusHandler);
 ```
+
+# 2. TODO List 생성과 삭제 (2023.1.18)
+dispatch를 이용해서 TODO를 생성하는것과 삭제를 만든다.
+여기서 단순히 html상에서 만들고 삭제되는것은 데이터가 남지 않으므로
+배열을 이용해서 데이터 스택을 만든다.
+
+```js
+import {createStore} from "redux";
+
+const getForm = document.querySelector('form');
+const getInput = document.querySelector('.inputVal');
+const getUl = document.querySelector('.LiWrap');
+
+const ADD = "Add";
+const MOD = "Mod";
+
+const onAdd = (data) =>{
+    return {type:ADD, val:data}
+}
+const onDel = (id) =>{
+    return {type:MOD, id}
+}
+const setReducer = (state = [],action) =>{
+    switch(action.type){
+        case ADD:
+            //push 하지말고(string 요소로 넣지말고)
+            //무조건 object(Array) 요소로 만들자.!
+            //스프레드 문법으로 state를 복사 하지 않으면 각각의 Array로 저장됨.(state로 데이터 스택을 쌓을수있다.)
+            return [{val:action.val, id:Date.now()},...state]
+        case MOD:
+            //filter 속성은 참인 값으로 구성된 새로운 배열을 생성한다.
+            //이걸 이용하여 state에 존재하는 배열에서 해당하는(지우고싶은) 요소를 빼낸다.
+            //그리고 그 뺴낸요소로 배열을 만드는데 필요 없는 배열이다.
+            return state.filter(dataItem => dataItem.id !== action.id)
+        default : return state;
+    }
+};
+const setDataStore = createStore(setReducer);
+
+const onDispatchAdd = (data) =>{
+    setDataStore.dispatch(onAdd(data));
+}
+const onDispatchDel = (e) =>{
+    const getDelTarget = parseInt(e.target.parentNode.id);
+    setDataStore.dispatch(onDel(getDelTarget));
+}
+
+const onPaint = () =>{
+    const getData = setDataStore.getState();
+    getUl.innerHTML = '';
+    getData.forEach(data=>{
+        const createLi = document.createElement('li');
+        const createBtn = document.createElement('button');
+        createBtn.innerHTML = 'Del';
+        createLi.id = data.id;
+        createLi.innerText = data.val;
+        getUl.appendChild(createLi);
+        createLi.appendChild(createBtn);
+        createBtn.addEventListener('click',onDispatchDel);
+    });
+}
+setDataStore.subscribe(()=>console.log(setDataStore.getState()));
+setDataStore.subscribe(onPaint);
+
+const onSubmit = (e) =>{
+    e.preventDefault();
+    const inputValue = getInput.value;
+    getInput.value = '';
+    onDispatchAdd(inputValue);
+}
+
+getForm.addEventListener('submit',onSubmit);
+```
--- src/index.js
+++ src/index.js
@@ -1,27 +1,65 @@
 import {createStore} from "redux";
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.getElementById('number');
+const getForm = document.querySelector('form');
+const getInput = document.querySelector('.inputVal');
+const getUl = document.querySelector('.LiWrap');
 
-const setReducer = (count = 0,action) =>{
-    if(action.type === 'Add'){
-        return count + 1;
-    } else if(action.type === 'Mod'){
-        return count - 1;
-    } else return count;
+const ADD = "Add";
+const MOD = "Mod";
+
+const onAdd = (data) =>{
+    return {type:ADD, val:data}
+}
+const onDel = (id) =>{
+    return {type:MOD, id}
+}
+const setReducer = (state = [],action) =>{
+    switch(action.type){
+        case ADD:
+            //push 하지말고(string 요소로 넣지말고)
+            //무조건 object(Array) 요소로 만들자.!
+            //스프레드 문법으로 state를 복사 하지 않으면 각각의 Array로 저장됨.(state로 데이터 스택을 쌓을수있다.)
+            return [{val:action.val, id:Date.now()},...state]
+        case MOD:
+            //filter 속성은 참인 값으로 구성된 새로운 배열을 생성한다.
+            //이걸 이용하여 state에 존재하는 배열에서 해당하는(지우고싶은) 요소를 빼낸다.
+            //그리고 그 뺴낸요소로 배열을 만드는데 필요 없는 배열이다.
+            return state.filter(dataItem => dataItem.id !== action.id)
+        default : return state;
+    }
 };
-const setCountStore = createStore(setReducer);
-const setSubscribe = () =>{
-    number.innerText = setCountStore.getState();
+const setDataStore = createStore(setReducer);
+
+const onDispatchAdd = (data) =>{
+    setDataStore.dispatch(onAdd(data));
 }
-setCountStore.subscribe(setSubscribe);
-const btnPlusHandler = () =>{
-    setCountStore.dispatch({type:'Add'})
-}
-const btnMinusHandler = () =>{
-    setCountStore.dispatch({type:'Mod'})
+const onDispatchDel = (e) =>{
+    const getDelTarget = parseInt(e.target.parentNode.id);
+    setDataStore.dispatch(onDel(getDelTarget));
 }
 
-add.addEventListener('click', btnPlusHandler);
-minus.addEventListener('click', btnMinusHandler);
+const onPaint = () =>{
+    const getData = setDataStore.getState();
+    getUl.innerHTML = '';
+    getData.forEach(data=>{
+        const createLi = document.createElement('li');
+        const createBtn = document.createElement('button');
+        createBtn.innerHTML = 'Del';
+        createLi.id = data.id;
+        createLi.innerText = data.val;
+        getUl.appendChild(createLi);
+        createLi.appendChild(createBtn);
+        createBtn.addEventListener('click',onDispatchDel);
+    });
+}
+setDataStore.subscribe(()=>console.log(setDataStore.getState()));
+setDataStore.subscribe(onPaint);
+
+const onSubmit = (e) =>{
+    e.preventDefault();
+    const inputValue = getInput.value;
+    getInput.value = '';
+    onDispatchAdd(inputValue);
+}
+
+getForm.addEventListener('submit',onSubmit);
